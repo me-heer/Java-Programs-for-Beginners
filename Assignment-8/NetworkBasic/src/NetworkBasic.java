ce3a8d1119739366f9bb07cd2f748f942979dda1
mformihir <50193156+mformihir@users.noreply.github.com>
2019-10-03 08:21:29 +0530

    Update NetworkBasic.java

--- Assignment-8/NetworkBasic/src/NetworkBasic.java
+++ Assignment-8/NetworkBasic/src/NetworkBasic.java
@@ -1,8 +1,12 @@
 import java.net.*;
+import java.util.Scanner;
 class NetworkBasic{
     public static void main(String[] args) throws UnknownHostException
     {
-        InetAddress address = InetAddress.getByName("www.google.com");
+        System.out.println("Enter domain name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        InetAddress address = InetAddress.getByName(name);
         System.out.println(address);  
     }
 }
