03daf8b6b6c68f96d390bf172514245c701a2cf4
Mihir Joshi <mihir67mj@gmail.com>
2019-12-11 11:12:28 +0530

    Organised
    Organised the projects into a folder.

--- Assignment-4/LMSProject/src/model/StaffUser.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class StaffUser extends User{
-    int experience;
-    StaffUser(String name, String branch, int id, int experience){
-      super(name, branch, id);
-      this.experience = experience;
-    }
-}
